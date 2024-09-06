8181f70a924257edc90c6fb1da2488d8f9644d46
César Cadillo <154623288+CesarCadillo@users.noreply.github.com>
2024-09-06 12:18:10 -0500

    Toda la tarea

---
+++ Ejercicio1_CesarCadillo/Ejercicio1_CesarCadillo/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio1_CesarCadillo
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}");
+        }
+    }
+}
